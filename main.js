f849f01d44ef6b5b1be632cee6619039081490a4
Anya Tran <panhtran249@gmail.com>
2015-05-05 15:33:05 -0400

    removed

--- main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  function returnHandleEvent() {
-
-  }
-  $('nav').fadeIn('slow');
-  console.log('hello');
-  $('.keyframe').css({'height' : $(window).height()});
-})
