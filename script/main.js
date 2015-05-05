007ba7c33be8bf1f8441e7ad6bac62d98e8b004e
Anya Tran <panhtran249@gmail.com>
2015-05-05 15:31:23 -0400

    n

---
+++ script/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  function returnHandleEvent() {
+
+  }
+  $('nav').fadeIn('slow');
+  console.log('hello');
+  $('.keyframe').css({'height' : $(window).height()});
+})
